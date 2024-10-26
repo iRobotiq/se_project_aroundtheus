d2a8405733d052515849f87dd4d658fa38ea77a8
GWEN THEEN <GTHEEN03@GMAIL.COM>
2024-10-25 20:54:44 -0400

    Fixed JS

--- scripts/index.js
+++ scripts/index.js
@@ -34,6 +34,10 @@ console.log(initialCards);
 
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const modalCloseButton = document.querySelector("#modal-close-button");
 profileEditButton.addEventListener("click", () => {
-  profileEditModal.classList.add("modal_opened");
+  profileEditModal.classList.add("modal__opened");
+});
+modalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
 });
